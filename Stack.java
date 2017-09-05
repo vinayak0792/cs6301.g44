a65fb54477934ad59d777ab7298cecfbd0872999
akshay <31359221+akshayrawat91@users.noreply.github.com>
2017-09-04 20:21:03 -0500

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,54 @@
+/**  Short Project 2: Program to implement array-based bounded-sized stack 
+ *   @author Akshay Rawat, Amrut Suresh , Gokul Surendra, Vinayaka Raju Gopal
+ * 
+ */
+
+package cs6301.g44;
+import java.util.EmptyStackException;
+
+public class Stack<T> {
+    final int size;
+    T[] bstack;
+    int top = -1;
+    
+    Stack(int size){
+        this.size = size;
+        bstack = new T[size-1];
+    }
+    
+    void push(T item) throws Exception{
+        int temp = top+1;
+        if(temp >= size){
+            throw new Exception("StackOverflow");
+        }
+        else{
+            bstack[temp] = item;
+            top++;
+        }
+    
+    }
+    
+    void pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        else{
+            top--;
+        }
+           
+    }
+    
+    public boolean isEmpty(){
+        if(top < 0)
+            return true;
+        else
+            return false;
+    }
+    
+    public String toString(){
+        for(int i = top;i >= 0; i--)
+            System.out.println((i+1)+" "+bstack[i]);
+        return null;
+    }
+    
+}
---
+++ stackImp.java
@@ -0,0 +1,20 @@
+/**  Short Project 2: Driver Program to perform operations on bounded-sized stack 
+ *   @author Akshay Rawat, Amrut Suresh , Gokul Surendra, Vinayaka Raju Gopal
+ * 
+ */
+
+package cs6301.g44;
+import java.util.Scanner;
+
+public class stackImp {
+
+    public static void main(String []args) throws Exception{
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the stack size");
+        Stack<String> stk = new Stack(in.nextInt());
+        stk.push("abc");
+        stk.push("xyz");
+        stk.toString();
+        
+    }
+}
