d0b152ef61afe9b1a40b45a3cf1d795cde442bc0
vinayak0792 <vinayak.0792@gmail.com>
2017-10-10 15:16:25 -0500

    SP7

---
+++ SP7/BST.java
@@ -0,0 +1,203 @@
+/** @author 
+ *  Binary search tree (starter code)
+ **/
+
+package cs6301.g44.SP7;
+
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class BST<T extends Comparable<? super T>> implements Iterable<T> {
+	static class Entry<T> {
+		T element;
+		Entry<T> left, right;
+
+		Entry(T x, Entry<T> left, Entry<T> right) {
+			this.element = x;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	Entry<T> root;
+	int size;
+	ArrayDeque<Entry<T>> ancestors = new ArrayDeque<>();
+
+	public BST() {
+		root = null;
+		size = 0;
+	}
+
+	// Find x in tree. Returns node where search ends.
+	Entry<T> find(T x) {
+		// ancestors.push(null);
+		return find(root, x);
+	}
+
+	// Helper function that looks for an element X in the BST.
+	// ancestor.peek() is parent of the root.
+	Entry<T> find(Entry<T> t, T x) {
+		if (t == null || t.element == x)
+			return t;
+		while (true) {
+			if (x.compareTo(t.element) < 0) {
+				if (t.left == null)
+					break;
+				else {
+					ancestors.push(t);
+					t = t.left;
+				}
+			} else if (t.element.compareTo(x) == 0)
+				break;
+			else {
+				if (t.right == null)
+					break;
+				else {
+					ancestors.push(t);
+					t = t.right;
+				}
+			}
+		}
+		return t;
+	}
+
+	/**
+	 * TO DO: Is x contained in tree?
+	 */
+	public boolean contains(T x) {
+		Entry<T> t = find(x);
+		return (t != null && t.element == x);
+	}
+
+	/**
+	 * TO DO: Is there an element that is equal to x in the tree? Element in
+	 * tree that is equal to x is returned, null otherwise.
+	 */
+	public T get(T x) {
+		return null;
+	}
+
+	/**
+	 * TO DO: Add x to tree. If tree contains a node with same key, replace
+	 * element by x. Returns true if x is a new element added to tree.
+	 */
+	public boolean add(T x) {
+		Entry<T> newElement = new Entry<T>(x, null, null);
+		if (root == null) {
+			root = newElement;
+			size++;
+		}
+		Entry<T> t = find(x);
+		if (t.element.compareTo(x) == 0) {
+			t.element = x;
+			return false;
+		} else if (x.compareTo(t.element) < 0)
+			t.left = newElement;
+		else
+			t.right = newElement;
+		size++;
+		return true;
+	}
+
+	/**
+	 * TO DO: Remove x from tree. Return x if found, otherwise return null
+	 */
+	public T remove(T x) {
+		if (root == null) {
+			return null;
+		}
+		Entry<T> t = find(x);
+		if (t.element.compareTo(x) != 0) {
+			return null;
+		}
+		T result = t.element;
+		if (t.left == null || t.right == null)
+			bypass(t);
+		else {
+			ancestors.push(t);
+			Entry<T> minRight = find(t.right, t.element);
+			t.element = minRight.element;
+			bypass(minRight);
+		}
+		size--;
+		return result;
+	}
+
+	void bypass(Entry<T> t) {
+		Entry<T> pt = ancestors.peek();
+		Entry<T> c = t.left == null ? t.right : t.left;
+		if (pt == null)
+			root = c;
+		else if (pt.left == t) {
+			pt.left = c;
+		} else
+			pt.right = c;
+
+	}
+
+	/**
+	 * TO DO: Iterate elements in sorted order of keys
+	 */
+	public Iterator<T> iterator() {
+		return null;
+	}
+
+	public static void main(String[] args) {
+		BST<Integer> t = new BST<>();
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			int x = in.nextInt();
+			if (x > 0) {
+				System.out.print("Add " + x + " : ");
+				t.add(x);
+				t.printTree();
+			} else if (x < 0) {
+				System.out.print("Remove " + x + " : ");
+				t.remove(-x);
+				t.printTree();
+			} //else {
+				// Comparable[] arr = t.toArray();
+				// System.out.print("Final: ");
+				// for (int i = 0; i < t.size; i++) {
+				// System.out.print(arr[i] + " ");
+				// }
+				// System.out.println();
+				// return;
+			}
+		//}
+	}
+
+	// TODO: Create an array with the elements using in-order traversal of tree
+	public Comparable[] toArray() {
+		Comparable[] arr = new Comparable[size];
+		/* write code to place elements in array here */
+		return arr;
+	}
+
+	public void printTree() {
+		System.out.print("[" + size + "]");
+		printTree(root);
+		System.out.println();
+	}
+
+	// Inorder traversal of tree
+	void printTree(Entry<T> node) {
+		if (node != null) {
+			printTree(node.left);
+			System.out.print(" " + node.element);
+			printTree(node.right);
+		}
+	}
+
+}
+/*
+ * Sample input: 1 3 5 7 9 2 4 6 8 10 -3 -6 -3 0
+ * 
+ * Output: Add 1 : [1] 1 Add 3 : [2] 1 3 Add 5 : [3] 1 3 5 Add 7 : [4] 1 3 5 7
+ * Add 9 : [5] 1 3 5 7 9 Add 2 : [6] 1 2 3 5 7 9 Add 4 : [7] 1 2 3 4 5 7 9 Add 6
+ * : [8] 1 2 3 4 5 6 7 9 Add 8 : [9] 1 2 3 4 5 6 7 8 9 Add 10 : [10] 1 2 3 4 5 6
+ * 7 8 9 10 Remove -3 : [9] 1 2 4 5 6 7 8 9 10 Remove -6 : [8] 1 2 4 5 7 8 9 10
+ * Remove -3 : [8] 1 2 4 5 7 8 9 10 Final: 1 2 4 5 7 8 9 10
+ * 
+ */
---
+++ SP7/SplayTree.java
@@ -0,0 +1,11 @@
+/** Starter code for Splay Tree
+ */
+package cs6301.g00;
+
+import java.util.Comparator;
+
+public class SplayTree<T extends Comparable<? super T>> extends BST<T> {
+    SplayTree() {
+	super();
+    }
+}
