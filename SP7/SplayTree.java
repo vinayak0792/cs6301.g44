d57d554892257e02ccf7a58ca09dc4f2ff5b7029
akshay <31359221+akshayrawat91@users.noreply.github.com>
2017-10-21 12:40:40 -0500

    update splay function & added parent var for Entry

--- SP7/SplayTree.java
+++ SP7/SplayTree.java
@@ -8,55 +8,58 @@ import java.util.Comparator;
 
 public class SplayTree<T extends Comparable<? super T>> extends BST<T> {
 	
+	static class Entry<T> extends BST.Entry<T>{
+		Entry<T> parent;	//to keep the info of parent of particular node
+		Entry(T x, Entry<T> left, Entry<T> right){
+			super(x,left,right);
+			parent = null;
+		}
+	}
+	
 	SplayTree(){
 		super();
-		
+	}
+	
+	Entry<T> getSplay(BST.Entry<T> t){
+		return (Entry<T>) t;
 	}
 	
 	public Entry<T> splay(Entry<T> node){
-		Entry<T> gt, pt;
-		Entry<T> tmp = root;
-		while(node != root) {
+		
+		while(node.parent != null) {
 			
-			Entry<T> n = find(node.element);
-//			pt = ancestors.pop();
-//			gt = ancestors.pop();
+			Entry<T> p = node.parent;	//parent of the node
+			Entry<T> gp = p.parent;		//grandparent of the node
 			
-			if(root.left.element.compareTo(node.element) == 0) {	//	Zig rotation
-				node = rightRotate(root);
+			if(gp == null) {
+				if(p.left.element.compareTo(node.element) == 0) {	//	Zig rotation
+					node = rightRotate(p);
+				}
+				else if(p.right.element.compareTo(node.element) == 0) {
+					node = leftRotate(p);
+				}
 			}
-			else if(root.right.element.compareTo(node.element) == 0) {
-				node = leftRotate(root);
-			}
-			else if(tmp.left.left.element.compareTo(node.element) == 0) {	//	Zig-Zig rotation
-				node = right2Rotate(tmp);
-			}
-			else if(tmp.right.right.element.compareTo(node.element) == 0) {
-				node = left2Rotate(tmp);
-			}
-			else if(tmp.left.right.element.compareTo(node.element) == 0 ) {	//	Zig-Zag rotation
-				node = leftrightRotate(tmp);
-			}
-			else if(tmp.right.left.element.compareTo(node.element) == 0) {
-				node = rightleftRotate(tmp);
+			else {
+				if(gp.left.left.element.compareTo(node.element) == 0) {	//	Zig-Zig rotation
+					node = right2Rotate(gp);
+				}
+				else if(gp.right.right.element.compareTo(node.element) == 0) {
+					node = left2Rotate(gp);
+				}
+				else if(gp.left.right.element.compareTo(node.element) == 0 ) {	//	Zig-Zag rotation
+					node = leftrightRotate(gp);
+				}
+				else if(gp.right.left.element.compareTo(node.element) == 0) {
+					node = rightleftRotate(gp);
+				}
 			}
 		}
-		 
+		root = node; 
 		return node;
 	}
 	
-	public boolean contains(T x) {
-		Entry<T> t = find(x);
-		Entry<T> node;
-		if(t != null && t.element == x) {
-			node = splay(t);
-		}
-		
-		return true;
-	}
-	
 	public Entry<T> rightRotate(Entry<T> node) {
-		Entry<T> child = node.left;
+		Entry<T> child = getSplay(node.left);
 		node.left = child.right;
 		child.right = node;
 		return child;
@@ -108,6 +111,47 @@ public class SplayTree<T extends Comparable<? super T>> extends BST<T> {
 		child.left = p;
 		child.right = gt;
 		return child;
+	}
+	
+	public boolean contains(T x) {
+		Entry<T> t = getSplay(find(x));
+		if(t != null && t.element == x) {
+			splay(t);
+		}
+		
+		return true;
+	}
+	
+	public boolean add(T x) {	// taken from AVLTree
+		Entry<T> newElement = new Entry<T>(x, null, null);
+
+		if (root == null) {
+			root = newElement;
+			newElement.parent = null;
+			size++;
+		}
+
+		Entry<T> t = getSplay(find(x));
+		if (t.element.compareTo(x) == 0) {
+			t.element = x;
+			return false;
+		} else if (x.compareTo(t.element) < 0) {
+			t.left = newElement;
+			newElement.parent = t;
+
+		} else {
+			t.right = newElement;
+			newElement.parent = t;
+		}
+
+		size++;
+		splay(newElement);
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		SplayTree<Integer> st = new SplayTree<>();
+		//st.add(1);
 	}
 }
 
