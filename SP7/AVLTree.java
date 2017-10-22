b12556b3c7b44251eff73726543283ac1f611928
vinayak0792 <vinayak.0792@gmail.com>
2017-10-22 14:37:46 -0500

    Final Update on BST & AVL Trees

--- SP7/AVLTree.java
+++ SP7/AVLTree.java
@@ -5,8 +5,6 @@ package cs6301.g44.SP7;
 
 import java.util.Scanner;
 
-import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
-
 public class AVLTree<T extends Comparable<? super T>> extends BST<T> {
 
 	static class Entry<T> extends BST.Entry<T> {
@@ -22,103 +20,72 @@ public class AVLTree<T extends Comparable<? super T>> extends BST<T> {
 		super();
 	}
 
-	int height(Entry<T> h) {
+	// Helper function that performs Right Rotation.
+	private Entry<T> rightRotate(Entry<T> node) {
+		Entry<T> temp1 = getAVLEntry(node.left);
+		Entry<T> temp2 = getAVLEntry(temp1.right);
+
+		temp1.right = node;
+		node.left = temp2;
+
+		// Updating the height of the subtree after rotation is completed.
+		node.height = Math.max(getHeight(getAVLEntry(node.left)), getHeight(getAVLEntry(node.right))) + 1;
+		temp1.height = Math.max(getHeight(getAVLEntry(temp1.left)), getHeight(getAVLEntry(temp1.right))) + 1;
+
+		return temp1;
+	}
+
+	private Entry<T> leftRotate(Entry<T> node) {
+		Entry<T> temp1 = getAVLEntry(node.right);
+		Entry<T> temp2 = getAVLEntry(temp1.left);
+
+		temp1.left = node;
+		node.right = temp2;
+
+		// Updating the height of the subtree after rotation is completed.
+		node.height = Math.max(getHeight(getAVLEntry(node.left)), getHeight(getAVLEntry(node.right))) + 1;
+		temp1.height = Math.max(getHeight(getAVLEntry(temp1.left)), getHeight(getAVLEntry(temp1.right))) + 1;
+		return temp1;
+
+	}
+
+	// Helper function that returns the height of the tree.
+	private int getHeight(Entry<T> h) {
 		if (h == null)
-			return 0;
+			return -1;
 
 		return h.height;
 	}
 
-	public Entry<T> rightRotate(Entry<T> node) {
-		Entry<T> temp1 = getAVLEntry(node.left);
-		Entry<T> temp2 = getAVLEntry(temp1.right);
-
-		// rotation
-		temp1.right = node;
-		node.left = temp2;
-
-		// updating height
-		// node.height = Math.max(height(getAVLEntry(node.left)),
-		// height(getAVLEntry(node.right))) + 1;
-		// temp1.height = Math.max(height(getAVLEntry(temp1.left)),
-		// height(getAVLEntry(temp1.right))) + 1;
-
-		return temp1;
-	}
-
-	public Entry<T> leftRotate(Entry<T> node) {
-		Entry<T> temp1 = getAVLEntry(node.right);
-		Entry<T> temp2 = getAVLEntry(temp1.left);
-
-		// rotation
-		temp1.left = node;
-		node.right = temp2;
-
-		// updating height
-		// node.height = Math.max(height(getAVLEntry(node.left)),
-		// height(getAVLEntry(node.right))) + 1;
-		// temp1.height = Math.max(height(getAVLEntry(temp1.left)),
-		// height(getAVLEntry(temp1.right))) + 1;
-		return temp1;
-
-	}
-
-	// Get Balance factor of ancestor
-	public int getBalance(Entry<T> parent) {
-		if (parent == null)
-			return 0;
-		else if (parent.left == null || parent.right == null) {
-			return parent.left == null ? -updateHeight(getAVLEntry(parent.right))
-					: updateHeight(getAVLEntry(parent.left));
-		} else {
-			return updateHeight(getAVLEntry(parent.left)) - updateHeight(getAVLEntry(parent.right));
-		}
-	}
-
-	int updateHeight(Entry<T> parent) {
+	// Helper function that calculates the balance factor of the subtree
+	private int getBalance(Entry<T> parent) {
 		if (parent == null)
 			return 0;
 
-		int leftHeight = updateHeight(getAVLEntry(parent.left));
-		int rightHeight = updateHeight(getAVLEntry(parent.right));
-
-		return Math.max(leftHeight, rightHeight) + 1;
-
+		return getHeight(getLeftChild(parent)) - getHeight(getRightChild(parent));
 	}
 
+	// Helper function that returns the equivalent AVLTree.Entry for the
+	// BST.Entry
 	Entry<T> getAVLEntry(BST.Entry<T> t) {
 		return (Entry<T>) t;
 	}
 
-	// void repair(int i, T x) {
-	// // System.out.println("\nroot height Before " +
-	// // getAVLEntry(root).height);
-	// updateHeight(i);
-	//
-	// // System.out.println("root height after " + getAVLEntry(root).height);
-	// int balance = getBalance(getAVLEntry(root));
-	//
-	// if (balance > 1 && x.compareTo(root.left.element) < 0)
-	// root = rightRotate(getAVLEntry(root));
-	//
-	// // Right Right Case
-	// if (balance < -1 && x.compareTo(root.right.element) > 0)
-	// root = leftRotate(getAVLEntry(root));
-	//
-	// // Left Right Case
-	// if (balance > 1 && x.compareTo(root.left.element) > 0) {
-	// root.left = leftRotate(getAVLEntry(root.left));
-	// root = rightRotate(getAVLEntry(root));
-	// }
-	//
-	// // Right Left Case
-	// if (balance < -1 && x.compareTo(root.right.element) < 0) {
-	// root.right = rightRotate(getAVLEntry(root.right));
-	// root = leftRotate(getAVLEntry(root));
-	// }
-	// }
+	// Helper function that returns the AVLTree.Entry type left child for
+	// BST.Entry type
+	Entry<T> getLeftChild(Entry<T> parent) {
+		return getAVLEntry(parent.left);
+	}
 
-	void updateParent(Entry<T> parent, T element) {
+	// Helper function that returns the AVLTree.Entry type right child for
+	// BST.Entry type
+	Entry<T> getRightChild(Entry<T> parent) {
+		return getAVLEntry(parent.right);
+	}
+
+	// Helper function that points the grandParent of the node after the
+	// respective rotation
+	private void updateParent(Entry<T> parent, T element) {
 		Entry<T> grandParent = getAVLEntry(ancestors.peek());
 		if (grandParent == null)
 			root = parent;
@@ -128,34 +95,56 @@ public class AVLTree<T extends Comparable<? super T>> extends BST<T> {
 			grandParent.right = parent;
 	}
 
-	public boolean add(T x) {
-		Entry<T> newElement = new Entry<T>(x, null, null);
+	// Helper function that performs rotation at the lowest node after deleting
+	// an entry from the tree.
+	private void repairDel(T x) {
+		while (ancestors.peek() != null && !ancestors.isEmpty()) {
+			Entry<T> parent = getAVLEntry(ancestors.pop());
+			Entry<T> leftChild = getLeftChild(parent);
+			Entry<T> rightChild = getRightChild(parent);
+			int balance = getBalance(parent);
 
-		if (root == null) {
-			root = newElement;
-			size++;
+			T element = parent.element;
+
+			// Left left Case
+			if (balance > 1 && getBalance(leftChild) >= 0) {
+				parent = rightRotate(getAVLEntry(parent));
+				updateParent(parent, element);
+			}
+			// Right Right Case
+			else if (balance < -1 && getBalance(rightChild) <= 0) {
+				parent = leftRotate(getAVLEntry(parent));
+				updateParent(parent, element);
+
+			}
+
+			// Left Right Case
+			else if (balance > 1 && getBalance(leftChild) < 0) {
+				parent.left = leftRotate(getAVLEntry(parent.left));
+				parent = rightRotate(getAVLEntry(parent));
+				updateParent(parent, element);
+			}
+
+			// Right Left Case
+			else if (balance < -1 && getBalance(rightChild) > 0) {
+				parent.right = rightRotate(getAVLEntry(parent.right));
+				parent = leftRotate(getAVLEntry(parent));
+				updateParent(parent, element);
+			}
+			parent.height = 1 + Math.max(getHeight(getAVLEntry(parent.left)), getHeight(getAVLEntry(parent.right)));
+
 		}
 
-		Entry<T> t = getAVLEntry(find(x));
-		if (t.element.compareTo(x) == 0) {
-			t.element = x;
-			return false;
-		} else if (x.compareTo(t.element) < 0) {
-			t.left = newElement;
+	}
 
-		} else {
-			t.right = newElement;
-		}
-
-		size++;
-		// System.out.println("\nroot height Before " +
-		// getAVLEntry(root).height);
-
-		while (!ancestors.isEmpty() && ancestors.peek() != null) {
-
+	// Helper function that performs rotation at the lowest node after adding an
+	// entry from the tree.
+	private void repairAdd(T x) {
+		while (ancestors.peek() != null && !ancestors.isEmpty()) {
 			Entry<T> parent = getAVLEntry(ancestors.pop());
 
 			int balance = getBalance(parent);
+
 			T element = parent.element;
 			// Left left Case
 			if (balance > 1 && x.compareTo(parent.left.element) < 0) {
@@ -182,10 +171,31 @@ public class AVLTree<T extends Comparable<? super T>> extends BST<T> {
 				parent = leftRotate(getAVLEntry(parent));
 				updateParent(parent, element);
 			}
+			parent.height = 1 + Math.max(getHeight(getAVLEntry(parent.left)), getHeight(getAVLEntry(parent.right)));
 
 		}
 
-		return true;
+	}
+
+	// Function to add a new element to the tree.
+	public boolean add(T x) {
+		Entry<T> newElement = new Entry<T>(x, null, null);
+		if (super.add(newElement) && size > 1) {
+			repairAdd(x);
+			return true;
+		} else
+			return false;
+	}
+
+	// Function to delete a new element to the tree.
+	public T remove(T x) {
+		T result = super.remove(x);
+		if (result != null) {
+			if (ancestors.peek() != null)
+				repairDel(ancestors.peek().element);
+			return result;
+		} else
+			return null;
 	}
 
 	public static void main(String[] args) {
@@ -196,17 +206,13 @@ public class AVLTree<T extends Comparable<? super T>> extends BST<T> {
 			if (x > 0) {
 				System.out.print("Add " + x + " : ");
 				t.add(x);
-				// t.repair(1, x);
-				System.out.println(t.root.element);
-				System.out.println("Is the tree balanced ? " + t.isBalanced(t.getAVLEntry(t.root)));
 				t.printTree();
+				System.out.println("Is the tree balanced ? " + t.isBalanced(t.getAVLEntry(t.root)));
 			} else if (x < 0) {
 				System.out.print("Remove " + x + " : ");
 				t.remove(-x);
-				// t.repair(-1, x);
-				System.out.println(t.root.element);
-				System.out.println("Is the tree balanced ? " + t.isBalanced(t.getAVLEntry(t.root)));
 				t.printTree();
+				System.out.println("Is the tree balanced ? " + t.isBalanced(t.getAVLEntry(t.root)));
 			} else {
 				Comparable<Integer>[] arr = t.toArray();
 				System.out.print("Final: ");
@@ -219,13 +225,15 @@ public class AVLTree<T extends Comparable<? super T>> extends BST<T> {
 		}
 	}
 
-	boolean isBalanced(Entry<T> root) {
+	// Function that checks if the tree is balanced post addition/deletion of an
+	// element.
+	private boolean isBalanced(Entry<T> root) {
 		int leftHeight;
 		int rightHeight;
 		if (root == null)
 			return true;
-		leftHeight = updateHeight(getAVLEntry(root.left));
-		rightHeight = updateHeight(getAVLEntry(root.right));
+		leftHeight = getHeight(getAVLEntry(root.left));
+		rightHeight = getHeight(getAVLEntry(root.right));
 
 		if (Math.abs(leftHeight - rightHeight) <= 1 && isBalanced(getAVLEntry(root.left))
 				&& isBalanced(getAVLEntry(root.right))) {
--- SP7/BST.java
+++ SP7/BST.java
@@ -26,7 +26,7 @@ public class BST<T extends Comparable<? super T>> implements Iterable<T> {
 	}
 
 	// Find x in tree. Returns node where search ends.
-	Entry<T> find(T x) {
+	public Entry<T> find(T x) {
 		ancestors = new Stack<Entry<T>>();
 		ancestors.push(null);
 		return find(root, x);
@@ -34,7 +34,7 @@ public class BST<T extends Comparable<? super T>> implements Iterable<T> {
 
 	// Helper function that looks for an element X in the BST.
 	// ancestor.peek() is parent of the root.
-	Entry<T> find(Entry<T> t, T x) {
+	public Entry<T> find(Entry<T> t, T x) {
 		if (t == null || t.element == x)
 			return t;
 		while (true) {
@@ -79,7 +79,7 @@ public class BST<T extends Comparable<? super T>> implements Iterable<T> {
 			return null;
 	}
 
-	boolean add(T x) {
+	public boolean add(T x) {
 		Entry<T> newElement = new Entry<T>(x, null, null);
 		if (add(newElement))
 			return true;
@@ -249,13 +249,3 @@ public class BST<T extends Comparable<? super T>> implements Iterable<T> {
 	}
 
 }
-/*
- * Sample input: 1 3 5 7 9 2 4 6 8 10 -3 -6 -3 0
- * 
- * Output: Add 1 : [1] 1 Add 3 : [2] 1 3 Add 5 : [3] 1 3 5 Add 7 : [4] 1 3 5 7
- * Add 9 : [5] 1 3 5 7 9 Add 2 : [6] 1 2 3 5 7 9 Add 4 : [7] 1 2 3 4 5 7 9 Add 6
- * : [8] 1 2 3 4 5 6 7 9 Add 8 : [9] 1 2 3 4 5 6 7 8 9 Add 10 : [10] 1 2 3 4 5 6
- * 7 8 9 10 Remove -3 : [9] 1 2 4 5 6 7 8 9 10 Remove -6 : [8] 1 2 4 5 7 8 9 10
- * Remove -3 : [8] 1 2 4 5 7 8 9 10 Final: 1 2 4 5 7 8 9 10
- * 
- */
