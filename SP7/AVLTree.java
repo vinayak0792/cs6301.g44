c2d9ae57d3d5cfaa2f0de0fc0ff05c343f360983
akshay <31359221+akshayrawat91@users.noreply.github.com>
2017-10-22 14:33:19 -0500

    Update AVLTree.java

--- SP7/AVLTree.java
+++ SP7/AVLTree.java
@@ -1,237 +1,224 @@
-
-/** Starter code for AVL Tree
+/*	Short Project 7: Implementation of bottom-up Splay Tree
+ *   @author Akshay Rawat, Amrut Suresh , Gokul Surendra, Vinayaka Raju Gopal
+ * 
  */
-package cs6301.g44.SP7;
+package cs6301.g44;
 
-import java.util.Scanner;
+import java.util.Comparator;
 
-import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
-
-public class AVLTree<T extends Comparable<? super T>> extends BST<T> {
-
-	static class Entry<T> extends BST.Entry<T> {
-		int height;
-
-		Entry(T x, Entry<T> left, Entry<T> right) {
-			super(x, left, right);
-			height = 0;
+public class SplayTree<T extends Comparable<? super T>> extends BST<T> {
+	
+	static class Entry<T> extends BST.Entry<T>{
+		Entry<T> parent;	//to keep the info of parent of particular node
+		Entry(T x, Entry<T> left, Entry<T> right){
+			super(x,left,right);
+			parent = null;
 		}
 	}
-
-	AVLTree() {
+	
+	SplayTree(){
 		super();
 	}
-
-	int height(Entry<T> h) {
-		if (h == null)
-			return 0;
-
-		return h.height;
-	}
-
-	public Entry<T> rightRotate(Entry<T> node) {
-		Entry<T> temp1 = getAVLEntry(node.left);
-		Entry<T> temp2 = getAVLEntry(temp1.right);
-
-		// rotation
-		temp1.right = node;
-		node.left = temp2;
-
-		// updating height
-		// node.height = Math.max(height(getAVLEntry(node.left)),
-		// height(getAVLEntry(node.right))) + 1;
-		// temp1.height = Math.max(height(getAVLEntry(temp1.left)),
-		// height(getAVLEntry(temp1.right))) + 1;
-
-		return temp1;
-	}
-
-	public Entry<T> leftRotate(Entry<T> node) {
-		Entry<T> temp1 = getAVLEntry(node.right);
-		Entry<T> temp2 = getAVLEntry(temp1.left);
-
-		// rotation
-		temp1.left = node;
-		node.right = temp2;
-
-		// updating height
-		// node.height = Math.max(height(getAVLEntry(node.left)),
-		// height(getAVLEntry(node.right))) + 1;
-		// temp1.height = Math.max(height(getAVLEntry(temp1.left)),
-		// height(getAVLEntry(temp1.right))) + 1;
-		return temp1;
-
-	}
-
-	// Get Balance factor of ancestor
-	public int getBalance(Entry<T> parent) {
-		if (parent == null)
-			return 0;
-		else if (parent.left == null || parent.right == null) {
-			return parent.left == null ? -updateHeight(getAVLEntry(parent.right))
-					: updateHeight(getAVLEntry(parent.left));
-		} else {
-			return updateHeight(getAVLEntry(parent.left)) - updateHeight(getAVLEntry(parent.right));
-		}
-	}
-
-	int updateHeight(Entry<T> parent) {
-		if (parent == null)
-			return 0;
-
-		int leftHeight = updateHeight(getAVLEntry(parent.left));
-		int rightHeight = updateHeight(getAVLEntry(parent.right));
-
-		return Math.max(leftHeight, rightHeight) + 1;
-
-	}
-
-	Entry<T> getAVLEntry(BST.Entry<T> t) {
+	
+	Entry<T> getSplay(BST.Entry<T> t){
 		return (Entry<T>) t;
 	}
-
-	// void repair(int i, T x) {
-	// // System.out.println("\nroot height Before " +
-	// // getAVLEntry(root).height);
-	// updateHeight(i);
-	//
-	// // System.out.println("root height after " + getAVLEntry(root).height);
-	// int balance = getBalance(getAVLEntry(root));
-	//
-	// if (balance > 1 && x.compareTo(root.left.element) < 0)
-	// root = rightRotate(getAVLEntry(root));
-	//
-	// // Right Right Case
-	// if (balance < -1 && x.compareTo(root.right.element) > 0)
-	// root = leftRotate(getAVLEntry(root));
-	//
-	// // Left Right Case
-	// if (balance > 1 && x.compareTo(root.left.element) > 0) {
-	// root.left = leftRotate(getAVLEntry(root.left));
-	// root = rightRotate(getAVLEntry(root));
-	// }
-	//
-	// // Right Left Case
-	// if (balance < -1 && x.compareTo(root.right.element) < 0) {
-	// root.right = rightRotate(getAVLEntry(root.right));
-	// root = leftRotate(getAVLEntry(root));
-	// }
-	// }
-
-	void updateParent(Entry<T> parent, T element) {
-		Entry<T> grandParent = getAVLEntry(ancestors.peek());
-		if (grandParent == null)
-			root = parent;
-		else if (grandParent.left.element == element)
-			grandParent.left = parent;
-		else
-			grandParent.right = parent;
+	
+	public void splay(Entry<T> node){
+		
+		while(node.parent != null) {
+			
+			Entry<T> p = node.parent;	//parent of the node
+			Entry<T> gp = p.parent;		//grandparent of the node
+			
+			if(gp == null) {
+				if(p.left.element.compareTo(node.element) == 0) {	//	Zig rotation
+					rightRotate(node,p);
+				}
+				else if(p.right.element.compareTo(node.element) == 0) {
+					leftRotate(node,p);
+				}
+			}
+			else {
+				if(gp.left.left.element.compareTo(node.element) == 0) {	//	Zig-Zig rotation
+					rightRotate(p,gp);
+					rightRotate(node,p);
+				}
+				else if(gp.right.right.element.compareTo(node.element) == 0) {
+					leftRotate(p,gp);
+					leftRotate(node,p);
+				}
+				else if(gp.left.right.element.compareTo(node.element) == 0 ) {	//	Zig-Zag rotation
+					leftRotate(p,gp);
+					rightRotate(node,p);
+				}
+				else if(gp.right.left.element.compareTo(node.element) == 0) {
+					rightRotate(p,gp);
+					leftRotate(node,p);
+				}
+			}
+		}
+		root = node; 
+		
 	}
-
-	public boolean add(T x) {
+	
+	public void rightRotate(Entry<T> child, Entry<T> p) {
+		//Entry<T> child = getSplay(p.left);
+		if(p.parent != null) {
+			if(p == p.parent.left)
+				p.parent.left = child;
+			else
+				p.parent.right = child;
+		}
+		if(child.right != null)
+			getSplay(child.right).parent = p;
+		
+		child.parent = p.parent;
+		p.parent = child;
+		p.left = child.right;
+		child.right = p;
+				
+	}
+	
+	public void leftRotate(Entry<T> child, Entry<T> p) {
+		//Entry<T> child = getSplay(p.right);
+		if(p.parent != null) {
+			if(p == p.parent.left)
+				p.parent.left = child;
+			else
+				p.parent.right = child;
+		}
+		if(child.left != null)
+			getSplay(child.left).parent = p;
+		
+		child.parent = p.parent;
+		p.parent = child;
+		p.right = child.left;
+		child.left = p;
+	}
+	
+//	public Entry<T> right2Rotate(Entry<T> gt){	//right-right
+//		Entry<T> p = gt.left;
+//		gt.left = p.right;
+//		p.right = gt;
+//		Entry<T> child = p.left;
+//		p.left = child.right;
+//		child.right = p;
+//		return child;
+//	}
+//	
+//	public Entry<T> left2Rotate(Entry<T> gt){	//left-left
+//		Entry<T> p = gt.right;
+//		gt.right = p.left;
+//		p.left = gt;
+//		Entry<T> child = p.right;
+//		p.right = child.left;
+//		child.left = p;
+//		return child;
+//	}
+//	
+//	public Entry<T> rightleftRotate(Entry<T> gt){	//right-left
+//		Entry<T> p = gt.right;
+//		Entry<T> child = p.left;
+//		gt.right = child.left;
+//		p.left = child.right;
+//		child.left = gt;
+//		child.right = p;
+//		return child;
+//	}
+//	
+//	public Entry<T> leftrightRotate(Entry<T> gt){	//left-right
+//		Entry<T> p = gt.left;
+//		Entry<T> child = p.right;
+//		gt.left = child.right;
+//		p.right = child.left;
+//		child.left = p;
+//		child.right = gt;
+//		return child;
+//	}
+	
+	public boolean contains(T x) {
+		Entry<T> t = getSplay(find(x));
+		if(t != null && t.element == x) {
+			splay(t);
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	public boolean add(T x) {	// taken from AVLTree
 		Entry<T> newElement = new Entry<T>(x, null, null);
 
 		if (root == null) {
 			root = newElement;
+			newElement.parent = null;
 			size++;
 		}
 
-		Entry<T> t = getAVLEntry(find(x));
+		Entry<T> t = getSplay(find(x));
 		if (t.element.compareTo(x) == 0) {
 			t.element = x;
 			return false;
 		} else if (x.compareTo(t.element) < 0) {
 			t.left = newElement;
+			newElement.parent = t;
 
 		} else {
 			t.right = newElement;
+			newElement.parent = t;
 		}
 
 		size++;
-		// System.out.println("\nroot height Before " +
-		// getAVLEntry(root).height);
-
-		while (!ancestors.isEmpty() && ancestors.peek() != null) {
-
-			Entry<T> parent = getAVLEntry(ancestors.pop());
-
-			int balance = getBalance(parent);
-			T element = parent.element;
-			// Left left Case
-			if (balance > 1 && x.compareTo(parent.left.element) < 0) {
-				parent = rightRotate(getAVLEntry(parent));
-				updateParent(parent, element);
-			}
-			// Right Right Case
-			else if (balance < -1 && x.compareTo(parent.right.element) > 0) {
-				parent = leftRotate(getAVLEntry(parent));
-				updateParent(parent, element);
-
-			}
-
-			// Left Right Case
-			else if (balance > 1 && x.compareTo(parent.left.element) > 0) {
-				parent.left = leftRotate(getAVLEntry(parent.left));
-				parent = rightRotate(getAVLEntry(parent));
-				updateParent(parent, element);
-			}
-
-			// Right Left Case
-			else if (balance < -1 && x.compareTo(parent.right.element) < 0) {
-				parent.right = rightRotate(getAVLEntry(parent.right));
-				parent = leftRotate(getAVLEntry(parent));
-				updateParent(parent, element);
-			}
-
-		}
-
+		splay(newElement);
 		return true;
 	}
-
-	public static void main(String[] args) {
-		AVLTree<Integer> t = new AVLTree<>();
-		Scanner in = new Scanner(System.in);
-		while (in.hasNext()) {
-			int x = in.nextInt();
-			if (x > 0) {
-				System.out.print("Add " + x + " : ");
-				t.add(x);
-				// t.repair(1, x);
-				System.out.println(t.root.element);
-				System.out.println("Is the tree balanced ? " + t.isBalanced(t.getAVLEntry(t.root)));
-				t.printTree();
-			} else if (x < 0) {
-				System.out.print("Remove " + x + " : ");
-				t.remove(-x);
-				// t.repair(-1, x);
-				System.out.println(t.root.element);
-				System.out.println("Is the tree balanced ? " + t.isBalanced(t.getAVLEntry(t.root)));
-				t.printTree();
-			} else {
-				Comparable<Integer>[] arr = t.toArray();
-				System.out.print("Final: ");
-				for (int i = 0; i < t.size; i++) {
-					System.out.print(arr[i] + " ");
-				}
-				System.out.println();
-				return;
-			}
+	
+	public T remove(T x) {
+		Entry<T> node = getSplay(find(x));
+		T result;
+		result = node.element;
+		if(node == null)
+			return null;
+		splay(node);
+		if(node.left != null && node.right != null) {
+			Entry<T> tmp = getSplay(node.left);
+			while(tmp.right != null)
+				tmp = getSplay(tmp.right);
+			
+			tmp.right = node.right;
+			getSplay(node.right).parent = tmp;
+			getSplay(node.left).parent = null;
+			root = node.right;
+			 
 		}
+		else if(node.right != null) {
+			getSplay(node.right).parent = null;
+			root = node.right;
+		}
+		else if(node.left != null) {
+			getSplay(node.left).parent = null;
+			root = node.left;
+		}
+		else {
+			root = null;
+		}
+		node.parent = null;
+        node.left = null;
+        node.right = null;
+        node = null;
+        return result;
 	}
-
-	boolean isBalanced(Entry<T> root) {
-		int leftHeight;
-		int rightHeight;
-		if (root == null)
-			return true;
-		leftHeight = updateHeight(getAVLEntry(root.left));
-		rightHeight = updateHeight(getAVLEntry(root.right));
-
-		if (Math.abs(leftHeight - rightHeight) <= 1 && isBalanced(getAVLEntry(root.left))
-				&& isBalanced(getAVLEntry(root.right))) {
-			return true;
-		} else
-			return false;
-
+	
+	public static void main(String[] args) {
+		SplayTree<Integer> st = new SplayTree<>();
+		//st.add(1);
 	}
 }
+
+
+
+
+
+
+
