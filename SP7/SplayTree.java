34477099d0f655d0ab42d90566b4d47a400bb2e2
akshay <31359221+akshayrawat91@users.noreply.github.com>
2017-10-22 14:49:48 -0500

    Update SplayTree.java

--- SP7/SplayTree.java
+++ SP7/SplayTree.java
@@ -97,46 +97,7 @@ public class SplayTree<T extends Comparable<? super T>> extends BST<T> {
 		child.left = p;
 	}
 	
-//	public Entry<T> right2Rotate(Entry<T> gt){	//right-right
-//		Entry<T> p = gt.left;
-//		gt.left = p.right;
-//		p.right = gt;
-//		Entry<T> child = p.left;
-//		p.left = child.right;
-//		child.right = p;
-//		return child;
-//	}
-//	
-//	public Entry<T> left2Rotate(Entry<T> gt){	//left-left
-//		Entry<T> p = gt.right;
-//		gt.right = p.left;
-//		p.left = gt;
-//		Entry<T> child = p.right;
-//		p.right = child.left;
-//		child.left = p;
-//		return child;
-//	}
-//	
-//	public Entry<T> rightleftRotate(Entry<T> gt){	//right-left
-//		Entry<T> p = gt.right;
-//		Entry<T> child = p.left;
-//		gt.right = child.left;
-//		p.left = child.right;
-//		child.left = gt;
-//		child.right = p;
-//		return child;
-//	}
-//	
-//	public Entry<T> leftrightRotate(Entry<T> gt){	//left-right
-//		Entry<T> p = gt.left;
-//		Entry<T> child = p.right;
-//		gt.left = child.right;
-//		p.right = child.left;
-//		child.left = p;
-//		child.right = gt;
-//		return child;
-//	}
-	
+
 	public boolean contains(T x) {
 		Entry<T> t = getSplay(find(x));
 		if(t != null && t.element == x) {
