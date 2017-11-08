d9478febcd68b6f99d6e40029553b8734288dad1
Amruth Suresh <amruth94@gmail.com>
2017-11-07 18:33:31 -0600

    Create LP4b.java

---
+++ LP4/LP4b.java
@@ -0,0 +1,16 @@
+package cs6301.g44;
+
+import cs6301.g00.Graph;
+
+public class LP4b {
+    static int VERBOSE = 0;
+    public static void main(String[] args) {
+	if(args.length > 0) { VERBOSE = Integer.parseInt(args[0]); }
+	Graph g = Graph.readDirectedGraph(new java.util.Scanner(System.in));
+	cs6301.g00.Timer t = new cs6301.g00.Timer();
+	LP4 handle = new LP4(g, null);
+	long result = handle.enumerateTopologicalOrders();
+	if(VERBOSE > 0) { LP4.printGraph(g, null, null, null, 0); }
+	System.out.println(result + "\n" + t.end());
+    }
+}
