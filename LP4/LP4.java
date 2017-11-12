7974434a9e44746d1bfe500f04297eed72f18a8a
Amruth Suresh <amruth94@gmail.com>
2017-11-11 23:43:43 -0600

    Update LP4.java

--- LP4/LP4.java
+++ LP4/LP4.java
@@ -5,17 +5,19 @@ import cs6301.g44.Graph.Vertex;
 import cs6301.g44.Graph.Edge;
 
 import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.ArrayDeque;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class LP4 {
 	Graph g;
 	Vertex s;
 
 	public static long count = 0; // number of topological orders
+	public static ArrayDeque<Vertex> bestPath=new ArrayDeque<>();
+	public static ArrayDeque<Vertex> bfsPath=new ArrayDeque<>();
 
 	// common constructor for all parts of LP4: g is graph, s is source vertex
 	public LP4(Graph g, Vertex s) {
@@ -25,55 +27,26 @@ public class LP4 {
 
 	// Part a. Return number of topological orders of g
 	public long countTopologicalOrders() {
-		int res = 0;
-		getcountTopologicalSorts(res);
+		Vertex[] res=new Vertex[g.n];
+		int index=0;
+		count = 0;
+		enumTopOrder(res,index,false);
 		return count;
-	}
-
-	public void getcountTopologicalSorts(int res) {
-		boolean flag = false; // to find whether all topological sorts are
-								// checked or not
-
-		for (Vertex x : g.v) {
-			if (x.inDegree == 0 && !x.visited) {
-				for (Edge ad : x.adj) {
-					ad.otherEnd(x).inDegree--;
-				}
-
-				res++;
-				x.visited = true;
-				getcountTopologicalSorts(res);
-
-				x.visited = false;
-				res--;
-
-				for (Edge ad : x.adj) {
-					ad.otherEnd(x).inDegree++;
-				}
-				flag = true;
-			}
-		}
-		if (!flag) {
-			if (res == g.n) {
-				count++;
-			}
-		}
 	}
 
 	// Part b. Print all topological orders of g, one per line, and
 	// return number of topological orders of g
 	public long enumerateTopologicalOrders() {
-		ArrayDeque<Vertex> result = new ArrayDeque<>();
 		Vertex[] res=new Vertex[g.n];
 		int index=0;
 		count = 0;
-		enumTopOrder(res,index);
+		enumTopOrder(res,index,true);
 		return count;
 	}
 
 	
 	
-	public void enumTopOrder(Vertex[] res,int index) {
+	public void enumTopOrder(Vertex[] res,int index,boolean partb) {
 		boolean flag = false; // to find whether all topological sorts are
 								// checked or not
 
@@ -86,7 +59,7 @@ public class LP4 {
 				res[index++]=x;
 				//result.addLast(x);
 				x.visited = true;
-				enumTopOrder(res,index);
+				enumTopOrder(res,index,partb);
 
 				x.visited = false;
 				//result.removeLast();
@@ -100,17 +73,17 @@ public class LP4 {
 		}
 
 		if (!flag) {
-			if(index==g.n){
+			if(partb){
 			for(int i=0;i<index;i++)
 				System.out.print(" "+res[i]);
-			}
+			
 				System.out.println();
+			}
 				count++;
 			}
 
 		}
 	
-
 	// Part c. Return the number of shortest paths from s to t
 	// Return -1 if the graph has a negative or zero cycle
 	public long countShortestPaths(Vertex t) {
@@ -127,52 +100,31 @@ public class LP4 {
 					e.disabled = true;
 			}
 		}
-
-		for (Vertex x : g.v) {
-			x.visited = false;
+		
+		List<Vertex> topList=new LinkedList<>();
+		topList=TopologicalOrder.toplogicalOrder2(g);
+		int i=0;
+		for(Vertex x:topList){
+			i++;
+			if(x.equals(t))
+				break;
 		}
-		System.out.println("bellmanford done");
-		count = 0;
-		dfsCount(s, t);
-
-		return count;
-	}
-
-	public void dfsPaths(Vertex src, Vertex dst, Vertex[] paths,int index) {
-		if (src.equals(dst)) {
-			count++;
-			paths[index++]=dst;
-			for(int i=0;i<index;i++)
-				System.out.print(paths[i]+" ");
-			System.out.println();
-			//paths.removeFirst();
-		} else {
-			src.visited = true;
-			//paths.push(src);
-			paths[index++]=src;
-			for (Edge e : src.adj) {
-				if (!e.otherEnd(src).visited && !e.disabled) {
-					dfsPaths(e.otherEnd(src), dst, paths,index);
+		topList=topList.subList(0, i);
+		for(Vertex v:g.v)
+			v.topCount=0;
+		
+		s.topCount=1;
+		long max=0;
+		
+		for(Vertex x:topList){
+				for(Edge e:x.adj){
+					if(!e.disabled){
+						e.otherEnd(x).topCount=e.otherEnd(x).topCount+x.topCount;
+						max=Math.max(max, e.otherEnd(x).count);
+					}
 				}
-			}
-			src.visited = false;
-			index--;
 		}
-	}
-
-	public void dfsCount(Vertex src, Vertex dst) {
-		if (src.equals(dst)) {
-			count++;
-		} else {
-			src.visited = true;
-
-			for (Edge e : src.adj) {
-				if (!e.otherEnd(src).visited && !e.disabled) {
-					dfsCount(e.otherEnd(src), dst);
-				}
-			}
-			src.visited = false;
-		}
+		return t.topCount;
 	}
 
 	public boolean bellmanFord(Vertex src) {
@@ -236,6 +188,28 @@ public class LP4 {
 		return count;
 	}
 
+	public void dfsPaths(Vertex src, Vertex dst, Vertex[] paths,int index) {
+		if (src.equals(dst)) {
+			count++;
+			paths[index++]=dst;
+			for(int i=0;i<index;i++)
+				System.out.print(paths[i]+" ");
+			System.out.println();
+			//paths.removeFirst();
+		} else {
+			src.visited = true;
+			//paths.push(src);
+			paths[index++]=src;
+			for (Edge e : src.adj) {
+				if (!e.otherEnd(src).visited && !e.disabled) {
+					dfsPaths(e.otherEnd(src), dst, paths,index);
+				}
+			}
+			src.visited = false;
+			index--;
+		}
+	}
+	
 	// Part e. Return weight of shortest path from s to t using at most k edges
 	public int constrainedShortestPath(Vertex t, int k) {
 		
@@ -268,78 +242,172 @@ public class LP4 {
 		}
 		return g.getVertex(t.getName() + 1).distance;
 	}
+	
+	class Bpath{
+		Vertex[] bpath=new Vertex[g.n];
+		int index;
+		int bestReward;
+		Vertex src,dst;
+	}
 
 	// Part f. Reward collection problem
 	// Reward for vertices is passed as a parameter in a hash map
 	// tour is empty list passed as a parameter, for output tour
 	// Return total reward for tour
 	public int reward(HashMap<Vertex, Integer> vertexRewardMap, List<Vertex> tour) {
-		// To do
-
-		ArrayDeque<Vertex> path = new ArrayDeque<>();
-		ArrayDeque<Vertex> bestPath = new ArrayDeque<>(); // stores path with
-															// best reward
-		ArrayDeque<Vertex> alternatePath = new ArrayDeque<>();
-
-		Vertex src, dst;
+		Bpath obj=new Bpath();
+		
+		Vertex src, dst,finalSource=null,finalDest=null;
+		int bestIndex=0;
 		src = this.s;
-		int highestReward = 0, tempReward;
-		Boolean res; // stores whether alternate path exists
-
+		int highestReward = 0, tempReward = 0;
+		
+		boolean nocycle = bellmanFord(s);
+		if (!nocycle)
+			return -1;
+		
 		for (Vertex v : g.v) {
 			if (!v.equals(src)) {
-				// System.out.println("source "+src);
-				// System.out.println("Destination "+v);
-				path.removeAll(path);
+				
 				dst = v;
 				dst.reward = 0;
-				shortestPath(src, dst);
-				Vertex tmp = dst;
-				while (tmp != src) {
-					path.addFirst(tmp);
-					tmp = tmp.parent;
+				
+				for(Vertex d:g.v){
+					d.reward=vertexRewardMap.get(d);
 				}
-				path.addFirst(src);
-
-				tempReward = 0;
-				for (Vertex s : path) {
-					tempReward = tempReward + vertexRewardMap.get(s);
-				}
-				// System.out.println("reward is "+tempReward);
-
-				for (Vertex w : path)
-					alternatePath.add(w);
-				res = checkAlternatePath(alternatePath, src, dst);
-
+				
+				Vertex[] temppath=new Vertex[g.n];
+				int index=0;
+				
+				obj.src=s;
+				obj.dst=dst;
+				bestPath(s, dst, temppath,index,0,obj,0);
+				
+					tempReward=obj.bestReward;
+				
+				
 				for (Vertex p : g.v) {
 					p.disabled = false;
 				}
 
-				// System.out.println("checking alternate path "+res+"\n");
-
-				if (res) {
 					if (tempReward > highestReward) {
-						highestReward = tempReward;
+						highestReward=tempReward;
 						bestPath.removeAll(bestPath);
-						for (Vertex l : path)
-							bestPath.add(l);
-						alternatePath.removeFirst();
-						for (Vertex l : alternatePath)
-							bestPath.add(l);
+						finalDest=obj.dst;
+						finalSource=obj.src;
+						for(int l=0;l<obj.index;l++){
+							bestPath.add(obj.bpath[l]);
+						}
+						bestIndex=obj.index;
 					}
 				}
-
-			}
-
 		}
-		// System.out.println("Highest reward "+highestReward);
-		// System.out.println("path is");
+		Vertex[] bb=new Vertex[bestIndex];
+		for(int i=0;i<bestIndex;i++){
+			bb[i]=bestPath.getFirst();
+		}
+		
+		//to get final bfs path for best path
+		checkAlternatePath(bb, bestIndex , finalSource, finalDest);
 		for (Vertex x : bestPath) {
-			// System.out.print(" "+x);
 			tour.add(x);
 		}
+		
+		for(Vertex d:bestPath)
+			d.disabled=true;
+		finalDest.disabled=false;
+		finalSource.disabled=false;
+		
+		bfs1(finalDest);
+		while(finalSource!=finalDest){
+			bfsPath.add(finalSource);
+			finalSource=finalSource.parent;
+		}
+		
+		Iterator<Vertex> it =bfsPath.descendingIterator(); 
+		while(it.hasNext())
+			tour.add(it.next());
+		
 		return highestReward;
 	}
+	
+	public void bestPath(Vertex src, Vertex dst, Vertex[] paths,int index,int maxReward,Bpath obj,int weight) {
+		if (src.equals(dst) && weight==dst.distance) {
+			count++;
+			paths[index++]=dst;
+			int tempReward=0;
+			for(int i=0;i<index;i++){
+				tempReward=tempReward+paths[i].reward;
+			}
+				if(tempReward>obj.bestReward && checkAlternatePath(paths,index,obj.src,obj.dst)){
+					maxReward=tempReward;
+					for(int i=0;i<index;i++){
+						obj.bpath[i]=paths[i];
+						obj.index=index;
+						obj.bestReward=tempReward;
+					}
+				}
+		} else {
+			src.visited = true;
+			paths[index++]=src;
+			for (Edge e : src.adj) {
+				if (!e.otherEnd(src).visited && !e.disabled) {
+					if(weight<=dst.distance){
+					weight=weight+e.weight;
+					bestPath(e.otherEnd(src), dst, paths,index,maxReward,obj,weight);
+					weight=weight-e.weight;
+					}
+				}
+			}
+			src.visited = false;
+			index--;
+		}
+	}
+	
+	public boolean checkAlternatePath(Vertex[] path, int index,Vertex src, Vertex dst) {
+
+		for (Vertex v : g.v) {
+			v.disabled = false;
+		}
+
+		for(int i=0;i<index;i++)
+			path[i].disabled=true;
+		
+		src.disabled = false;
+		dst.disabled = false;
+		
+		bfs1(dst);
+		boolean res; 
+		if(src.seen)
+			res=true;
+		else
+			res=false;
+		
+		return res;
+	}
+	
+	public void bfs1(Vertex src) {
+		for(Vertex v:g.v)
+			v.seen=false;
+		Queue<Vertex> q = new LinkedList<Vertex>();
+		src.seen=true;
+		q.add(src);
+		Vertex temp,dec;
+		while(!q.isEmpty()){
+			temp=q.remove();
+			if(!temp.disabled){
+				for(Edge e:temp.adj){
+					dec=e.otherEnd(temp);
+					if(!dec.seen){
+						dec.seen=true;
+						q.add(dec);
+						dec.parent=temp;
+					}
+						
+				}
+			}
+		}
+	  }
 
 	// Do not modify this function
 	static void printGraph(Graph g, HashMap<Vertex, Integer> map, Vertex s, Vertex t, int limit) {
@@ -365,97 +433,6 @@ public class LP4 {
 			System.out.println("Limit: " + limit + " edges");
 		}
 		System.out.println("___________________________________");
-	}
-
-	public void shortestPath(Vertex src, Vertex dst) {
-
-		Comparator<Vertex> cmp = new Comparator<Graph.Vertex>() {
-
-			@Override
-			public int compare(Vertex o1, Vertex o2) {
-				if (o1.distance < o2.distance)
-					return -1;
-				else if (o1.distance > o2.distance)
-					return 1;
-				else
-					return 0;
-			}
-		};
-		PriorityQueue<Vertex> pq = new PriorityQueue<>(cmp);
-		g.getVertex(src.getName() + 1).distance = 0;
-		for (Vertex x : g.v) {
-			if (x.getName() != src.getName())
-				x.distance = Integer.MAX_VALUE;
-			x.sptSet = false;
-			pq.add(x);
-		}
-
-		while (!pq.isEmpty()) {
-			Vertex temp = pq.poll();
-			temp.sptSet = true;
-
-			if (temp.getName() == dst.getName()) // if this is destination node
-				break;
-
-			for (Edge v : temp.adj) {
-				Vertex oth = v.otherEnd(temp);
-				if (!oth.sptSet && temp.distance != Integer.MAX_VALUE && temp.distance + v.weight < oth.distance) {
-					oth.distance = temp.distance + v.weight;
-					oth.parent = temp;
-					pq.remove(oth);
-					pq.add(oth);
-				}
-			}
-		}
-	}
-
-	public boolean checkPath(ArrayDeque<Vertex> path, BFS b) {
-		Vertex temp = path.peekLast();
-		while (temp != path.peek()) {
-			// System.out.println("Parent of "+temp+" is "+b.getParent(temp));
-			temp = b.getParent(temp);
-			if (temp == null)
-				return false;
-		}
-		if (temp.equals(path.peek()))
-			return true;
-		else
-			return false;
-	}
-
-	public boolean checkAlternatePath(ArrayDeque<Vertex> path, Vertex src, Vertex dst) {
-
-		for (Vertex v : g.v) {
-			v.disabled = false;
-		}
-
-		for (Vertex x : path) {
-			x.disabled = true;
-		}
-		src.disabled = false;
-		dst.disabled = false;
-
-		// System.out.println("Running BFS");
-		BFS b = new BFS(g, dst);
-		b.bfs();
-
-		ArrayDeque<Vertex> BFSpath = new ArrayDeque<>();
-		Vertex temp = src;
-		while (temp != dst) {
-			BFSpath.addFirst(temp);
-			temp = b.getParent(temp);
-			if (temp == null)
-				break;
-		}
-		BFSpath.addFirst(dst);
-
-		boolean res = checkPath(BFSpath, b);
-
-		path.removeAll(path);
-		for (Vertex r : BFSpath)
-			path.add(r);
-
-		return res;
 	}
 
 }
